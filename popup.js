5eaf60c65dd5976c0811ece5227570d9d7fe1b6c
Poway <mischadewalsjes@gmail.com>
2023-02-13 13:43:08 +0100

    basis widget

--- popup.js
+++
@@ -1,6 +0,0 @@
-// document.querySelector("#show-reserveren").addEventListener("click", function(){
-//     document.querySelector(".popup").classList.add("active");
-//   });
-//   document.querySelector(".popup .close-button").addEventListener("click", function(){
-//     document.querySelector(".popup").classList.remove("active");
-//   });
